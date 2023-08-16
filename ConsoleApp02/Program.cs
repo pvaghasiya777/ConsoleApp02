d800ee538b3132086cac40a584e77b1b92d72051
pvaghasiya777 <paragvaghasiya777@gmail.com>
2023-08-16 16:54:11 +0530

    Add project files.

---
+++ ConsoleApp02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary01;
+
+
+namespace ConsoleApp02
+{
+    class GetSum
+    {
+        static void Main(string[] args)
+        {
+            Class1 ob = new Class1();
+           // ob.Run();
+
+            Console.WriteLine("Sum :-  "+ ob.SUM(35,22));
+            ob.Display("Hello From Console App");
+            ob.Display("I am feeling fantastic and full of positivity today!");
+            Console.Read();
+
+        }
+            
+    }
+}
