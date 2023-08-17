a3e7b64ecc304f53f5e156fa158ceef593ca1bd0
pvaghasiya777 <paragvaghasiya777@gmail.com>
2023-08-17 14:25:27 +0530

    #Add Lib File

---
+++ ClassLibrary01/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ClassLibrary01
+{
+    public class Class1
+    {
+
+        int i;
+
+        public int SUM(int a,int b) 
+        {
+            i = a + b;
+            Console.WriteLine("First Number of Sum is "+ a +" And Second Number of Sum Is "+b);
+            return i; 
+        }
+
+        public void Display(string msg)
+        {
+            Console.WriteLine("Your Display Message From Console App is :- "+ msg);
+        }
+    }
+}
